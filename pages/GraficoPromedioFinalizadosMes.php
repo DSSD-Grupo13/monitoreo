90355681017f355f868144d324414be3b8f1056b
Ortu Agustin <ortu.agustin@gmail.com>
2017-11-27 00:49:44 -0300

    Los casos son finalizados o pendientes, no importa si fueron aceptados o no.. flasheamos en la metrica esta

--- pages/GraficoPromedioFinalizadosMes.php
+++ pages/GraficoPromedioFinalizadosMes.php
@@ -4,25 +4,25 @@ require '../includes/config.php';
 
 $casos_archivados = Cases::getArchivedCases()['data'];
 $total_casos = count($casos_archivados) + Cases::getCountCases();
-$casos_aprobados_mes_corriente = 0;
+$finalizados = 0;
 foreach ($casos_archivados as $case) {
   $inicio = new \DateTime($case->start);
   $fin = new \DateTime($case->end_date);
   $diff = $fin->diff($inicio);
   if ($diff->d <= 30)
-    $casos_aprobados_mes_corriente++;
+    $finalizados++;
 }
 
-$casos_rechazados_mes_corriente = $total_casos - $casos_aprobados_mes_corriente;
-$promedio = $casos_aprobados_mes_corriente / $total_casos;
+$pendientes = $total_casos - $finalizados;
+$promedio = $finalizados / $total_casos;
 ?>
 
 <div id = "chart_1" style="width:100%; height:400px;"> </div>
 <center>
-  <div>  Aprobados: <?php echo $casos_aprobados_mes_corriente ?> </div>
-  <div>  Rechazados: <?php echo $casos_rechazados_mes_corriente ?>  </div>
-  <div>  Total: <?php echo $total_casos ?> </div>
-  <div>  Promedio: <?php echo round($promedio * 100, 2) ?> %  </div>
+  <div> Finalizados: <?php echo $finalizados ?> </div>
+  <div> Pendientes: <?php echo $pendientes ?>  </div>
+  <div> Total: <?php echo $total_casos ?> </div>
+  <div> Promedio: <?php echo round($promedio * 100, 2) ?> %  </div>
 </center>
 
 <script>
@@ -35,10 +35,10 @@ $promedio = $casos_aprobados_mes_corriente / $total_casos;
           text: 'Casos finalizados en el mes que inician'
       },
       xAxis: {
-          categories: ['Aprobados, Rechazados']
+          categories: ['Finalizados, Pendientes']
       },
       series: [{
-        data: [{name:'Aprobados',y: <?php echo $casos_aprobados_mes_corriente ?>}, {name:'Rechazados', y: <?php echo $casos_rechazados_mes_corriente ?>}]
+        data: [{name:'Finalizados',y: <?php echo $finalizados ?>}, {name:'Pendientes', y: <?php echo $pendientes ?>}]
       }]
   });
 });
